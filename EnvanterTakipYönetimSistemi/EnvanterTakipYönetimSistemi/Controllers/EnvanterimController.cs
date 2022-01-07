64b5e337bf878e392a4e31f852a2686daa4b8c77
Mustafa Beyazbulut <77739828+mustafabeyazbulut@users.noreply.github.com>
2022-01-07 13:07:48 +0300

    talep ve servis sayfası eksik

--- EnvanterTakipYönetimSistemi/EnvanterTakipYönetimSistemi/Controllers/EnvanterimController.cs
+++ EnvanterTakipYönetimSistemi/EnvanterTakipYönetimSistemi/Controllers/EnvanterimController.cs
@@ -1,4 +1,6 @@
 ﻿using EnvanterTakipYönetimSistemi.Login;
+using EnvanterTakipYönetimSistemi.Models;
+using EnvanterTakipYönetimSistemi.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,13 +9,46 @@ using System.Web.Mvc;
 
 namespace EnvanterTakipYönetimSistemi.Controllers
 {
+    
     public class EnvanterimController : Controller
     {
+
+        private Models.EnvanterServisEntities db = new Models.EnvanterServisEntities();
         // GET: Envanterim
         [LoginControl]
-        public ActionResult Index()
+        public ActionResult Index(EnvanterimViewModel model)
         {
-            return View();
+            int kullaniciID = Convert.ToInt32(Session["Per_ID"]);
+            model.EnvanterimList = (from x in db.Tbl_Zimmet.Where(k=>k.Kullanan_ID==kullaniciID).OrderByDescending(f => f.Zim_ID)
+                                select new EnvanterimViewModel
+                                {
+                                    Zimmet_ID = x.Zim_ID,
+                                    Z_EnvanterID = (int)x.Env_ID,
+                                    Z_EnvanterCinsAdi = x.Tbl_Envanter.Tbl_P_EnvanterCinsi.EnvCins_Adi,
+                                    Z_EnvanterMarkaAdi = x.Tbl_Envanter.Tbl_P_EnvanterMarka.EnvMarka_Adi,
+                                    Z_EnvanterModel = x.Tbl_Envanter.Env_Model,
+                                    Z_EnvanterSeriNo = x.Tbl_Envanter.Env_SeriNo,
+                                    ZimmetTarih = (DateTime)x.Zim_Tarih,
+                                    ZimmetIadeTarih = (DateTime)x.Zim_IadeTarih,
+                                    ZimmetAciklama = x.Zim_Aciklama,
+                                    ZimmetKayit = (bool)x.Zim_Kayit
+                                }).ToList();
+
+
+            if (Request.IsAjaxRequest()) // index sayfası ilk defa açılmadıysa sadece tabloyu günceller
+            {
+                return PartialView("_EnvanterTablo", model);
+            }
+            else
+            {
+                return View(model);
+            }
+        }
+
+        [HttpPost]
+        public string EnvanterimDestek()
+        {
+            return "1";
         }
     }
 }
---
+++ EnvanterTakipYönetimSistemi/EnvanterTakipYönetimSistemi/ViewModels/EnvanterimViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnvanterTakipYönetimSistemi.ViewModels
+{
+    public class EnvanterimViewModel
+    {
+
+        public int Zimmet_ID { get; set; }
+        public int Z_EnvanterID { get; set; }
+        public string Z_EnvanterCinsAdi { get; set; }
+        public string Z_EnvanterMarkaAdi { get; set; }
+        public string Z_EnvanterModel { get; set; }
+        public string Z_EnvanterSeriNo { get; set; }
+        public DateTime ZimmetTarih { get; set; }
+        public DateTime ZimmetIadeTarih { get; set; }
+        public string ZimmetAciklama { get; set; }
+        public bool ZimmetKayit { get; set; }
+
+        public List<EnvanterimViewModel> EnvanterimList { get; set; }
+
+    }
+}
