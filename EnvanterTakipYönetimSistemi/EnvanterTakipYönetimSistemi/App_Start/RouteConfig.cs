9e9fdc7b6203181e1825551e743e278c89e7e77d
Mustafa Beyazbulut <77739828+mustafabeyazbulut@users.noreply.github.com>
2022-01-07 04:39:00 +0300

    .

--- EnvanterTakipYönetimSistemi/EnvanterTakipYönetimSistemi/App_Start/RouteConfig.cs
+++ EnvanterTakipYönetimSistemi/EnvanterTakipYönetimSistemi/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace EnvanterTakipYönetimSistemi
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
             );
+
+            //           routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
---
+++ EnvanterTakipYönetimSistemi/EnvanterTakipYönetimSistemi/Controllers/EnvanterimController.cs
@@ -0,0 +1,19 @@
+﻿using EnvanterTakipYönetimSistemi.Login;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnvanterTakipYönetimSistemi.Controllers
+{
+    public class EnvanterimController : Controller
+    {
+        // GET: Envanterim
+        [LoginControl]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
